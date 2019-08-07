c84078f5f3209cdcc7fc879e6959f414dcb2e76c
John <johnguinezm@gmail.com>
2019-08-07 00:35:29 -0400

    RestServer - usuario

--- server/config/config.js
+++ server/config/config.js
@@ -2,3 +2,21 @@
 // Puerto
 //================================================
 process.env.PORT = process.env.PORT || 3000
+
+//================================================
+// Entorno
+//================================================
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev'
+
+//================================================
+// Base de Datos
+//================================================
+let urlDB;
+
+if (process.env.NODE_ENV === 'deev') {
+    urlDB = 'mongodb://localhost:27017/cafe'
+} else {
+    urlDB = "mongodb+srv://striker:mLBIfhMpkHkV3ha1@cluster0-a04ju.mongodb.net/cafe";
+}
+
+process.env.URLDB = urlDB;
--- server/server.js
+++ server/server.js
@@ -1,7 +1,11 @@
 require('./config/config')
 
-
 const express = require('express')
+const mongoose = require('mongoose');
+
+// Using ES6 imports
+// import mongoose from 'mongoose';
+
 const app = express()
 
 var bodyParser = require('body-parser')
@@ -12,37 +16,17 @@ app.use(bodyParser.urlencoded({ extended: false }))
 // parse application/json
 app.use(bodyParser.json())
 
-app.get('/usuario', function(req, res) {
-    res.json('Get Usuario');
-})
+app.use(require('./routes/usuario'))
 
-app.post('/usuario', function(req, res) {
-    let body = req.body;
+mongoose.connect(process.env.URLDB, { useNewUrlParser: true, useCreateIndex: true },
+    (err, res) => {
 
-    if (body.nombre === undefined) {
-        res.status(400).json({
-            ok: false,
-            mensaje: 'el nombre es necesario'
-        });
-    } else {
-        res.json({
-            persona: body
-        });
-    }
+        if (err) throw err;
 
-})
+        console.log('Base de Datos ONLINE');
 
-app.put('/usuario/:id', function(req, res) {
-    let id = req.params.id;
-    res.json({ id });
-})
-
-app.delete('/usuario', function(req, res) {
-    res.json('delete Usuario')
-})
-
+    });
 
 app.listen(process.env.PORT, () => {
     console.log('escuchando puerto', process.env.PORT);
-
 })
